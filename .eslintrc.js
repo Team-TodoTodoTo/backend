5b54e0e11462ee62bbf1b7ca57f6bd1100ba6a20
hangyeol <snake011128@naver.com>
2025-01-18 20:03:45 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // 추가
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
